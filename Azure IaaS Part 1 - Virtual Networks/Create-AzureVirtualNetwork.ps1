23e501402b224a29be56033909bc987dbd7b5e89
Martyn Coupland <martyn.coupland@inframon.com>
2016-04-14 09:47:06 +0100

    Initial commit

---
+++ Azure IaaS Part 1 - Virtual Networks/Create-AzureVirtualNetwork.ps1
@@ -0,0 +1,18 @@
+﻿# Login to Azure
+#Login-AzureRmAccount
+
+# Get the resource group
+$ResourceGroup = Get-AzureRmResourceGroup -ResourceGroupName "TheCloudGuy"
+
+# Create the VNet
+New-AzureRmVirtualNetwork -ResourceGroupName $($ResourceGroup.ResourceGroupName) -Name VN-TheCloudGuy -Location $($ResourceGroup.Location) -AddressPrefix 172.16.0.0/20
+
+# Retrieve the VNet
+$VNet = Get-AzureRmVirtualNetwork -ResourceGroupName $($ResourceGroup.ResourceGroupName) -Name VN-TheCloudGuy
+
+# Add subnets
+Add-AzureRmVirtualNetworkSubnetConfig -Name GatewaySubnet -VirtualNetwork $VNet -AddressPrefix 172.16.1.0/26
+Add-AzureRmVirtualNetworkSubnetConfig -Name VirtualMachines -VirtualNetwork $VNet -AddressPrefix 172.16.2.0/24
+
+# Save the configuration
+Set-AzureRmVirtualNetwork -VirtualNetwork $VNet
